a3fb11fd60dbc654e8d88ba865e3a3c313af88db
imamiya-masaki <imachan567@gmail.com>
2021-02-24 16:49:58 +0900

    複数ファイル読み込みbutton作成wip

---
+++ src/components/ReadFile.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ReadFile">
+    <b-form-file v-model="file" class="mt-3" @change="readFile()" multiple directory plain></b-form-file>
+    <div class="mt-3">Selected file: {{ file ? file.name : '' }}</div>
+    <button @click="readFile()">確認</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReadFile',
+  props: {
+  },
+  data () {
+    return {
+      file: null
+    }
+  },
+  methods: {
+    readFile: function () {
+      console.log('file', this.file)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.ReadFile {
+}
+</style>
