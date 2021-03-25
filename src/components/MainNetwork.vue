1ebb2ce9c3915e81f1774e2c5f9715787b188221
imamiya-masaki <imamiyagit@gmail.com>
2021-03-26 06:12:18 +0900

    network描画用の箱用意

---
+++ src/components/MainNetwork.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="MainNetwork" id="cy">
+  </div>
+</template>
+
+<script>
+import cytoscape from 'cytoscape';
+
+export default {
+  name: 'ReadFile',
+  props: {
+  },
+  data () {
+    return {
+      fileUpperCamelCase: true, // 後々ここフラグによって変化させたいので
+      ReadFileOnly: false // 読み込んだファイルのみをネットワーク表示させる
+    }
+  },
+  methods: {
+  },
+  watch: {
+    counter () {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .MainNetwork {
+  border: 0.5px solid;
+ }
+</style>
