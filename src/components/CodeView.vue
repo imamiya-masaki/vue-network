4ae31d638d970bdce09f413f05748d05333217ce
imamiya-masaki <imamiyagit@gmail.com>
2021-04-11 21:41:14 +0900

    導入

---
+++ src/components/CodeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="CodeView">
+    <pre>
+    </pre>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'CodeView',
+  components: {
+  },
+  props: {
+    code: {
+      default: '',
+      type: String
+    }
+  },
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    onClick: function () {
+      this.visible = !this.visible
+    }
+  },
+  mounted: function () {
+    const prettyPrint = require('code-prettify')
+    console.log('preti', prettyPrint)
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/RightBar.vue
+++ src/components/RightBar.vue
@@ -7,17 +7,20 @@
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
           in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         </p>
-        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
       </div>
+      <code-view />
     </b-sidebar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import CodeView from '@/components/CodeView'
+
 export default {
   name: 'Home',
   components: {
+    CodeView
   },
   props: {
     buttonVisible: {
