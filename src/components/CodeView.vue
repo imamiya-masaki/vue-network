92341f1ad3b76dca51b7bb3bc4d6a4586f983cd3
imamiya-masaki <imamiyagit@gmail.com>
2021-04-11 22:38:48 +0900

    wip

--- src/components/CodeView.vue
+++ src/components/CodeView.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="CodeView">
-    <pre>
-    </pre>
+    <code-highlight language="Markup">
+      {{ code }}
+    </code-highlight>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import CodeHighlight from 'vue-code-highlight/src/CodeHighlight.vue'
+import 'vue-code-highlight/themes/prism-okaidia.css'
 export default {
   name: 'CodeView',
   components: {
+    CodeHighlight
   },
   props: {
     code: {
--- src/components/RightBar.vue
+++ src/components/RightBar.vue
@@ -8,7 +8,7 @@
           in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
         </p>
       </div>
-      <code-view />
+      <code-view :code="sample"/>
     </b-sidebar>
   </div>
 </template>
@@ -30,7 +30,23 @@ export default {
   },
   data () {
     return {
-      visible: false
+      visible: false,
+      sample: `
+      <template>
+  <div class="right_bar">
+    <b-button v-if="buttonVisible" @click="onClick">Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-right" title="Sidebar"  v-model="visible" right shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+      </div>
+      <code-view />
+    </b-sidebar>
+  </div>
+</template>
+`
     }
   },
   methods: {
